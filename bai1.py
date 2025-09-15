f01faffdd70483c9adae02a70df5fc00fbc098d3
dolsck <dinhquoctuan859@gmail.com>
2025-09-15 09:04:00 +0700

    tao lam xong bai1 roi

---
+++ bai1.py
@@ -0,0 +1,27 @@
+import math 
+
+nhapvao = input()
+nhapvao2 = input()
+
+a = int(nhapvao) 
+b = int(nhapvao2)
+
+def func(a,b):
+    result = 0
+
+    if a == 0: result = b**2
+    elif a == 1: result = math.sqrt(b)
+    elif a == 2: result = b
+
+    return result
+
+y = func(a,b)
+
+u1 = b**2 * y
+u2 = math.sqrt(b) * y
+u3 = b * y
+
+print(u1," ",u2," ",u3)
+
+result = 1e1000
+print(result)
